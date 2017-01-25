e3b080b1ae0f746332e50e07a37820fe1ce5ff45
Alan Semenov <ase@enonic.com>
2017-01-25 11:49:26 +0100

    Fixed build config

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -16,5 +16,5 @@ module.exports = {
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
   ],
-  navigateFallback: 'index.html',
+  navigateFallback: 'index.html'
 };
